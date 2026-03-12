0d87d24fdb3728abf4f6f0cad20e20027163ebbf
Alejandro Garcia <lz19020045@zacatecassur.tecnm.mx>
2026-03-12 11:03:09 -0600

    Init ios

---
+++ IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by Devel Cit on 12/03/26.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
